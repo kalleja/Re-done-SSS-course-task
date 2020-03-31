921c1bcf933438e173c8161a88b73b00ac1b6c9f
kalleja <kalle.jansson@metropolia.fi>
2020-03-31 16:46:19 +0300

    Update app.js

--- week2/app.js
+++ week2/app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const feeling = require('./routes/catRoute');
+const user = require('./routes/peopleRoute');
 
 
 app.use('/cat', feeling);
+app.use('/user', userRouter);
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
